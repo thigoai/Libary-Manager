84bf6f2814eb121231048c9f7b55fbf960862360
thigoai <150091259+thigoai@users.noreply.github.com>
2024-11-25 22:16:36 -0300

    Update Repositorio.java

--- Repositorio.java
+++ Repositorio.java
@@ -1,5 +1,35 @@
 package br.ufrn.imd.modelo;
 
-public class Repositorio<T extends Publicacao> {
+import java.util.ArrayList;
+
+public class Repositorio{
+	
+	private ArrayList<Publicacao> publicacoes;
+
+	public Repositorio() {
+		publicacoes = new ArrayList<Publicacao> ();
+	}
+	
+	public void adicionarPublicacao(Publicacao p) {
+		publicacoes.add(p);
+	}
+	
+	public void listarPublicacoes() {
+		for (Publicacao p : publicacoes) {
+			System.out.println("------------------------------------");
+			p.exibirInforBasica();
+		}
+	} 
+	
+	public Publicacao procurarPorTitulo(String titulo) {
+		for (Publicacao p : publicacoes) {
+			if(p.getTitulo() == titulo) {
+				return p;
+			}
+		}
+		System.out.println("Item nao encontrado");
+		return null;
+	}
+	
 
 }
